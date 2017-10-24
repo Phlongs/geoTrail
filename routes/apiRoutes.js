ba6ac37d0f5def40679df16e73beef3d7fa4796d
Phlongs <phlongs@gmail.com>
2017-10-24 16:11:38 -0400

    logic updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const router = new express.Router();
 var unirest = require('unirest');
-var myip = require('quick-local-ip');
-var where = require('node-where');
 
-var location = {};
-var ip = myip.getLocalIP6();
+var loc = {};
 
-where.is(ip, function(err, result) {
-	console.log(ip)
-  if (result) {
-    console.log('City: ' + result.get('city'));
-    console.log('State / Region: ' + result.get('region'));
-    console.log('State / Region Code: ' + result.get('regionCode'));
-    console.log('Zip: ' + result.get('postalCode'));
-    console.log('Country: ' + result.get('country'));
-    console.log('Country Code: ' + result.get('countryCode'));
-    console.log('Lat: ' + result.get('lat'));
-    console.log('Lng: ' + result.get('lng'));
-    location.lat = result.get('lat');
-    location.lng = result.get('lng');
-  }
+router.post('/location', function(req,res){
+	loc.lat = req.body.lat;
+	loc.lng = req.body.lng;
+
+	console.log(loc.lat)
 });
 
 router.get('/', function(req, res)
 {
-
-	var url = 'https://trailapi-trailapi.p.mashape.com/?lat=' + location.lat + 
-	'&limit=25&lon=' + location.lng + '&q[activities_activity_type_name_eq]=hiking&radius=50';
+	console.log(loc)
+	console.log(req.body)
+	var url = 'https://trailapi-trailapi.p.mashape.com/?lat=' + loc.lat + 
+	'&limit=25&lon=' + loc.lng + '&q[activities_activity_type_name_eq]=hiking&radius=50';
 
 	unirest.get(url)
 	.header("X-Mashape-Key", "hOkRc76SJAmshN9SZOyT3raU7QpGp1uQqprjsnczSazK2uuEQK")
@@ -36,10 +25,14 @@ router.get('/', function(req, res)
 		  var trails = trails.body;
 		  res.render('index', {
 		  	trails : trails,
-		  	location : location
+		  	location: loc.lat
 		  }); 
 	});
   
 });
+
+
+
+
 
 module.exports = router;
